78fc1a66cead04334af9e8be4e6a34c05f374162
BrenoCamaro <breno.camaro.5@gmail.com>
2024-09-22 11:33:35 -0300

    Experimeto 1 finalzado

--- Main.java
+++ Main.java
@@ -8,14 +8,18 @@ public class Main {
         File pasta = new File(caminhoPasta);
         File[] arquivos = pasta.listFiles(); //listFiles retorna os arquivos no diretório
 
+        long startTime = System.currentTimeMillis(); // Captura o tempo inicial
 
         for(File arquivo:arquivos) {
-
             if(arquivo.getName().endsWith(".csv")) {
                 LeitorCsv leitor = new LeitorCsv(arquivo);
                 leitor.leitura();
             }
-
         }
+
+        long endTime = System.currentTimeMillis(); // Captura o tempo final
+        long duration = endTime - startTime; // Calcula a duração em milissegundos
+
+        System.out.println("O tempo de execução foi: " + duration + " milissegundos");
     }
 }
