5a52661cbffa16d811ada0e9e4b2b830ad749549
BrenoCamaro <breno.camaro.5@gmail.com>
2024-09-22 07:18:31 -0300

    Implementando a funcionalidade de calculo das temperaturas na classe LeitorCsv.java

--- LeitorCsv.java
+++ LeitorCsv.java
@@ -24,16 +24,38 @@ public class LeitorCsv {
         try(BufferedReader leitor = new BufferedReader(new FileReader(arquivo))) {
             String linha;
 
+            leitor.readLine(); // Ignora a primeira linha (cabeçalho)
+
+            ArrayList<Float> listaDeTemp = new ArrayList<>();
+
             while((linha = leitor.readLine()) != null) {
-                String[]valores = linha.split(","); // Divide a linha pelos valores separados por vírgula
-                System.out.println(Arrays.toString(valores)); // Exibe os valores no console
+                String[] valores = linha.split(","); // Divide a linha pelos valores separados por vírgula
+                
+                String temp = valores[5]; // Pegando a temperatura
+
+                listaDeTemp.add(Float.parseFloat(temp)); // Adiciona a temperatura convertida para Float à lista
             }
 
-        }catch(IOException e) {
+            //Cálculo da temperatura mínima e máxima
+            float temperaturaMin = Collections.min(listaDeTemp);
+            float temperaturaMax = Collections.max(listaDeTemp);
+
+            //Cálculo da média
+            float soma = 0;
+            for(float temperatura : listaDeTemp) {
+                soma += temperatura;
+            }
+
+            float temperaturaMedia = soma / listaDeTemp.size();
+
+            System.out.printf("Temperatura mínima: %f\n", temperaturaMin);
+            System.out.printf("Temperatura máxima: %f\n", temperaturaMax);
+            System.out.printf("Temperatura média: %f\n", temperaturaMedia);
+
+        } catch(IOException e) {
             e.printStackTrace();
+        } catch(NumberFormatException e) {
+            System.out.println("Erro ao converter temperatura para número: " + e.getMessage());
         }
     }
-
-    
-    
 }
